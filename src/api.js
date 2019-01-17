97916f9d7d8a3b46f3c9a2bed28987e8aa8cef3e
tl <xtonylaw@gmail.com>
2019-01-17 16:54:09 +0800

    添加错误判断

--- src/api.js
+++ src/api.js
@@ -3,5 +3,9 @@ import fetch from "isomorphic-fetch";
 export function fetchCircuits( ) {
     return fetch( "http://ergast.com/api/f1/2019/circuits.json" )
         .then( res => res.json( ) )
-        .then( res => res.MRData.CircuitTable.Circuits );
+        .then( res => res.MRData.CircuitTable.Circuits )
+        .catch( err => {
+            console.log( err );
+            return [];
+        } );
 }
